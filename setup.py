a16d7895e39871b5af02275451b1f2a4f8645cff
pvankessel <pvankessel@pewresearch.org>
2017-03-17 09:00:35 -0400

    modified setup.py to include static files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django_learning',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Complete Django framework for manual and automated document classification, machine learning, and more',
     long_description = 'http://labs.pewresearch.tech/docs/libs/django_learning',
     url = 'https://github.com/pewresearch/django_learning',
@@ -11,9 +11,11 @@ setup(
     install_requires = [
         'django',
         'pewtils',
+        'django_commander',
         'pandas'
     ],
     packages = find_packages(exclude = ['contrib', 'docs', 'tests']),
+    include_package_data=True,
     classifiers = [
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 1 - Planning',
