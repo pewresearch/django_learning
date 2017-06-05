db06df770eaf27c27d4f736b3ffb7b990126bb34
pvankessel <pvankessel@pewresearch.org>
2017-06-05 13:36:34 -0400

    removed print

--- django_learning/__init__.py
+++ django_learning/__init__.py
@@ -3,4 +3,4 @@ import os
 with open(os.path.join(os.path.dirname(__file__), "VERSION"), "rb") as version_file:
     __version__ = version_file.read().strip()
 
-print "Using django_learning from: {}".format(os.path.dirname(__file__))
+# print "Using django_learning from: {}".format(os.path.dirname(__file__))
