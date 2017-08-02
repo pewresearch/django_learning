55e748c20258b4c7026f85db62be48e82df21563
pvankessel <pvankessel@pewresearch.org>
2017-08-02 14:41:39 -0400

    removed print

--- django_learning/context_processors.py
+++ django_learning/context_processors.py
@@ -3,7 +3,6 @@ from django.conf import settings
 
 def identify_template(request):
 
-    print settings.DJANGO_LEARNING_BASE_TEMPLATE
     return {
         "django_learning_template": settings.DJANGO_LEARNING_BASE_TEMPLATE
     }
