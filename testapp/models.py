ce22792fac998957ebf6aef99efed4f4badde6f7
pvankessel <pvankessel@pewresearch.org>
2019-04-04 10:20:44 -0400

    added test app

---
+++ testapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+from django_pewtils.abstract_models import BasicExtendedModel
+
+
+class MovieReview(BasicExtendedModel):
+
+    document = models.OneToOneField("django_learning.Document", related_name="movie_review")
