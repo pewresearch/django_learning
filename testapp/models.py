9d1ac3e139444494c7b652daad50feae91234a9f
pvankessel <pvankessel@pewresearch.org>
2019-09-26 14:35:05 -0400

    updated testapp for py3

--- testapp/models.py
+++ testapp/models.py
@@ -6,5 +6,8 @@ from django_pewtils.abstract_models import BasicExtendedModel
 class MovieReview(BasicExtendedModel):
 
     document = models.OneToOneField(
-        "django_learning.Document", related_name="movie_review"
+        "django_learning.Document",
+        related_name="movie_review",
+        on_delete=models.SET_NULL,
+        null=True,
     )
