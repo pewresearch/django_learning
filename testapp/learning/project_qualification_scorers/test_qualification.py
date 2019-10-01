d6073a5282c140cf69a7aa765220f83e34f47700
pvankessel <pvankessel@pewresearch.org>
2019-10-01 17:02:17 -0400

    updated unit tests

--- testapp/learning/project_qualification_scorers/test_qualification.py
+++ testapp/learning/project_qualification_scorers/test_qualification.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import
+
+
 def scorer(qual_assignment):
 
     return int(qual_assignment.codes.get(label__question__name="q1").label.value) == 1
--- testapp/learning/topic_models/test.py
+++ testapp/learning/topic_models/test.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import
+
+
 def get_parameters():
 
     return {
