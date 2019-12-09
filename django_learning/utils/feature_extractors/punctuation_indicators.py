60b7a43986a1399da3a3b7390ec0f588b32e6cd8
pvankessel <pvankessel@pewresearch.org>
2019-12-09 15:25:04 -0500

    update punctuation indicator extractor

--- django_learning/utils/feature_extractors/punctuation_indicators.py
+++ django_learning/utils/feature_extractors/punctuation_indicators.py
@@ -14,26 +14,27 @@ class Extractor(BasicExtractor):
             ("dollars", r"\$([0-9]{1,3}(?:(?:\,[0-9]{3})+)?(?:\.[0-9]{1,2})?)\s"),
             ("dollars_alt", r"\$[0-9]{1,3}((\,[0-9]{3})+)?\s"),
             ("amounts", r"thousand*|million*|billion*|hundred*"),
+            ("exclamation", r"\!"),
         ]
 
         super(Extractor, self).__init__(*args, **kwargs)
 
     def transform(self, X, **transform_params):
 
-        # text = X['text']
-        # for p in self.get_preprocessors():
-        #     text = text.apply(p.run)
+        text = X["text"]
+        for p in self.get_preprocessors():
+            text = text.apply(p.run)
 
         regexes = [(n, re.compile(r, re.IGNORECASE)) for n, r in self.regexes]
         rows = []
-        for index, r in X.iterrows():
+        for text in text:
             row = []
             for name, regex in regexes:
-                matches = [m for m in regex.findall(r["text"]) if m != ""]
+                matches = [m for m in regex.findall(text) if m != ""]
                 row.extend([len(matches), 1 if len(matches) > 0 else 0])
             rows.append(row)
 
-        return pandas.DataFrame(rows)
+        return pandas.DataFrame(rows, columns=self.get_feature_names())
 
     def fit(self, X, y=None, **fit_params):
 
