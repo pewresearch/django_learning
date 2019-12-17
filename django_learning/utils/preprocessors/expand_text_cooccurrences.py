9257694cf6d5a6b32e4b6e5f23c3faf7f874116a
pvankessel <pvankessel@pewresearch.org>
2019-12-17 13:33:01 -0500

    updated preprocessor name

--- django_learning/utils/preprocessors/expand_text_cooccurrences.py
+++ django_learning/utils/preprocessors/expand_text_cooccurrences.py
@@ -6,7 +6,7 @@ from django_learning.utils.preprocessors import BasicPreprocessor
 class Preprocessor(BasicPreprocessor):
     def __init__(self, *args, **kwargs):
 
-        self.name = "clean_text"
+        self.name = "expand_text_cooccurrences"
         super(Preprocessor, self).__init__(*args, **kwargs)
 
     def run(self, text):
