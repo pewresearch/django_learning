772db85767db0105d7870415caca4d161779ca3f
pvankessel <pvankessel@pewresearch.org>
2019-12-17 13:32:14 -0500

    removed regression models since thats not implemented yet

--- django_learning/utils/models/regression/random_forest.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-from sklearn.ensemble import RandomForestRegressor
-
-
-def get_params():
-
-    return {"model_class": RandomForestRegressor(), "params": {"n_estimators": (10,)}}
