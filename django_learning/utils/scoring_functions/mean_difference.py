eaf7031d1bfd1956f0588d6b390665f72a122ecf
pvankessel <pvankessel@pewresearch.org>
2019-12-19 15:24:57 -0500

    cleaned up scorers

--- django_learning/utils/scoring_functions/mean_difference.py
+++ django_learning/utils/scoring_functions/mean_difference.py
@@ -10,6 +10,3 @@ def scorer(y_true, y_pred, sample_weight=None):
     lower = min(avgs)
     higher = max(avgs)
     return lower / higher
-    # return 1.0 / numpy.average([int(y) for y in y_true], weights=sample_weight) - numpy.average([int(y) for y in y_pred], weights=sample_weight)
-    # print "True avg: {}, pred avg: {}".format(numpy.average([int(y) for y in y_true], weights=sample_weight), numpy.average([int(y) for y in y_pred], weights=sample_weight))
-    # return 1.0 / abs(numpy.average([int(y) for y in y_true], weights=sample_weight) / numpy.average([int(y) for y in y_pred], weights=sample_weight))
