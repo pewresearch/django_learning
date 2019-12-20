28748d8e997fddc788d5581e7a3031a898cd947d
pvankessel <pvankessel@pewresearch.org>
2019-12-20 11:06:24 -0500

    fixed a couple of unit test bugs

--- testapp/learning/dataset_code_filters/test.py
+++ testapp/learning/dataset_code_filters/test.py
@@ -1,6 +1,13 @@
 from __future__ import absolute_import
 
+from django_pewtils import get_model
+
 
 def filter(self, df, *args, **kwargs):
-
-    return df[df["label_id"] == 12]
+    pk = (
+        get_model("Question", app_name="django_learning")
+        .objects.get(name="test_checkbox")
+        .labels.get(value="1")
+        .pk
+    )
+    return df[df["label_id"] == pk]
