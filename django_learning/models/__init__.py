88fe7ed32c91a6fbf1ce5e2a3754342f997fb265
pvankessel <pvankessel@pewresearch.org>
2020-04-16 14:01:06 -0400

    adding automatic util module imports so everything is always loaded, to avoid pickling errors

--- django_learning/models/__init__.py
+++ django_learning/models/__init__.py
@@ -10,3 +10,24 @@ from django_learning.models.topics import *
 from django_learning.models.word2vec import *
 from django_learning.models.classification import *
 from django_learning.models.regression import *
+
+from django_learning.utils.balancing_variables import balancing_variables
+from django_learning.utils.dataset_code_filters import dataset_code_filters
+from django_learning.utils.dataset_coder_filters import dataset_coder_filters
+from django_learning.utils.dataset_document_filters import dataset_document_filters
+from django_learning.utils.dataset_extractors import dataset_extractors
+from django_learning.utils.feature_extractors import feature_extractors
+from django_learning.utils.models import models
+from django_learning.utils.pipelines import pipelines
+from django_learning.utils.preprocessors import preprocessors
+from django_learning.utils.project_hit_types import project_hit_types
+from django_learning.utils.project_qualification_scorers import project_qualification_scorers
+from django_learning.utils.project_qualification_tests import project_qualification_tests
+from django_learning.utils.regex_filters import regex_filters
+from django_learning.utils.regex_replacers import regex_replacers
+from django_learning.utils.sampling_frames import sampling_frames
+from django_learning.utils.sampling_methods import sampling_methods
+from django_learning.utils.scoring_functions import scoring_functions
+from django_learning.utils.stopword_sets import stopword_sets
+from django_learning.utils.stopword_whitelists import stopword_whitelists
+from django_learning.utils.topic_models import topic_models
