54d12a91cc2e3dc000ad4e12c452728b91d201d6
Brian Broderick <brian@broderick.io>
2021-08-02 19:56:01 +0000

    version file issues

--- django_learning/__init__.py
+++ django_learning/__init__.py
@@ -1,8 +1,6 @@
 from builtins import str
 import os
 
-with open(os.path.join(os.path.dirname(__file__), "VERSION"), "rb") as version_file:
-    __version__ = str(version_file.read().strip())
 
 default_app_config = "django_learning.apps.DjangoLearningConfig"
 
