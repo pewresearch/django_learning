c9cb10fb9fd75de2636bf6601bffb8e3d54ecab6
pvankessel <pvankessel@pewresearch.org>
2021-12-08 16:53:22 -0500

    Add exception handling for datetime vs. date type comparisons

--- django_learning/utils/dataset_document_filters/filter_by_date.py
+++ django_learning/utils/dataset_document_filters/filter_by_date.py
@@ -1,8 +1,14 @@
 def filter(self, df, min_date=None, max_date=None):
 
     if min_date:
-        df = df[df["date"] >= min_date]
+        try:
+            df = df[df["date"] >= min_date]
+        except TypeError:
+            df = df[df["date"].dt.date >= min_date]
     if max_date:
-        df = df[df["date"] <= max_date]
+        try:
+            df = df[df["date"] <= max_date]
+        except TypeError:
+            df = df[df["date"].dt.date <= max_date]
 
     return df
