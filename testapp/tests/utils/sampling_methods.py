cc2e36d6e71bff3b216813841457551a2534341a
pvankessel <pvankessel@pewresearch.org>
2021-12-10 13:09:14 -0500

    Update sampling frame tests

--- testapp/tests/utils/sampling_methods.py
+++ testapp/tests/utils/sampling_methods.py
@@ -8,7 +8,7 @@ from django_learning.models import *
 from django_commander.commands import commands
 
 from testapp.models import MovieReview
-from testapp.utils import set_up_test_project
+from testapp.utils import set_up_test_project, set_up_test_sample
 
 
 class SamplingMethodsTests(DjangoTestCase):
@@ -18,7 +18,7 @@ class SamplingMethodsTests(DjangoTestCase):
     """
 
     def setUp(self):
-        set_up_test_project()
+        set_up_test_project(50)
 
     def test_sampling_methods(self):
         from django_learning.utils.sampling_methods import sampling_methods
@@ -26,6 +26,14 @@ class SamplingMethodsTests(DjangoTestCase):
         for val in ["keyword_oversample"]:
             self.assertIn(val, sampling_methods.keys())
             self.assertIsNotNone(sampling_methods[val]())
+
+        set_up_test_sample("test_sample_keyword_oversample", 4)
+        self.assertEqual(
+            Sample.objects.get(name="test_sample_keyword_oversample")
+            .documents.filter(text__iregex="action")
+            .count(),
+            2,
+        )
 
     def tearDown(self):
 
