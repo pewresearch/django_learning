50e0389ecfb68753c5d1c2e680569cced6a75018
pvankessel <pvankessel@pewresearch.org>
2021-12-13 14:16:53 -0500

    Add more documentation and docstrings

--- django_learning/models/ngram_sets.py
+++ django_learning/models/ngram_sets.py
@@ -7,11 +7,30 @@ from django_learning.managers import NgramSetManager
 
 
 class NgramSet(LoggedExtendedModel):
+    """
+    Represents a set of words that belong to a given category, e.g. a sentiment dicitonary. The category has a ``name``,
+    and may belong to a specific ``dictionary`` (a broader set of categories). Name and dictionary should, together,
+    be unique. Can optionally be given a more verbose ``label``, and the words that belong to the category exist
+    in the ``words`` array field.
+    """
 
-    name = models.CharField(max_length=100, db_index=True)
-    dictionary = models.CharField(max_length=100, db_index=True)
-    label = models.CharField(max_length=100, db_index=True, null=True)
-    words = ArrayField(models.CharField(max_length=50), default=list)
+    name = models.CharField(
+        max_length=100, db_index=True, help_text="Short name of the category"
+    )
+    dictionary = models.CharField(
+        max_length=100, db_index=True, help_text="Dictionary the category belongs to"
+    )
+    label = models.CharField(
+        max_length=100,
+        db_index=True,
+        null=True,
+        help_text="More verbose name for the category",
+    )
+    words = ArrayField(
+        models.CharField(max_length=50),
+        default=list,
+        help_text="List of words in the category",
+    )
 
     objects = NgramSetManager().as_manager()
 
@@ -24,15 +43,26 @@ class NgramSet(LoggedExtendedModel):
 
 
 class DocumentNgramSet(LoggedExtendedModel):
+    """
+    An application of an ngram set on a specific document.
+    """
 
     ngram_set = models.ForeignKey(
-        "django_learning.NgramSet", related_name="documents", on_delete=models.CASCADE
+        "django_learning.NgramSet",
+        related_name="documents",
+        on_delete=models.CASCADE,
+        help_text="The ngram set that was applied to the document",
     )
     document = models.ForeignKey(
-        "django_learning.Document", related_name="ngram_sets", on_delete=models.CASCADE
+        "django_learning.Document",
+        related_name="ngram_sets",
+        on_delete=models.CASCADE,
+        help_text="The document the NgramSet was applied to",
     )
-    count = models.IntegerField()
-    percent = models.FloatField()
+    count = models.IntegerField(help_text="Number of matches in the document")
+    percent = models.FloatField(
+        help_text="Proportion of the document that matched to the ngram set"
+    )
 
     class Meta:
         unique_together = ("ngram_set", "document")
---
+++ docs_source/models/topics.rst
@@ -0,0 +1,22 @@
+Topics
+===========
+
+TopicModel
+~~~~~~~~~~~~~~~~~~~~~
+
+.. autoclass:: django_learning.models.topics.TopicModel
+
+Topic
+~~~~~~~~~~~~~~~~~~~~~
+
+.. autoclass:: django_learning.models.topics.Topic
+
+TopicNgram
+~~~~~~~~~~~~~~~~~~~~~
+
+.. autoclass:: django_learning.models.topics.TopicNgram
+
+DocumentTopic
+~~~~~~~~~~~~~~~~~~~~~
+
+.. autoclass:: django_learning.models.topics.DocumentTopic
