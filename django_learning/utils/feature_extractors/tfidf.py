29bc28e5e4f5b6e680e417b3c97aca1746b18051
Gonzalo Rivero <grivero@pewresearch.org>
2022-01-04 18:41:36 -0500

    Delete unused wmom import

--- django_learning/utils/feature_extractors/tfidf.py
+++ django_learning/utils/feature_extractors/tfidf.py
@@ -3,7 +3,6 @@ from collections import defaultdict
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from django_learning.utils import wmom
 from django_pewtils import get_model
 
 from django_learning.utils.feature_extractors import BasicExtractor
