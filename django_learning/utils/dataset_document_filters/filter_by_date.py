55314569bd367a8178dc6eedde5378f581953029
Gonzalo Rivero <grivero@pewresearch.org>
2022-01-04 17:15:40 -0500

    Comment with explanation of Exception

--- django_learning/utils/dataset_document_filters/filter_by_date.py
+++ django_learning/utils/dataset_document_filters/filter_by_date.py
@@ -4,6 +4,10 @@ def filter(self, df, min_date=None, max_date=None):
         try:
             df = df[df["date"] >= min_date]
         except TypeError:
+            ## TypeError arises 'naturally' if date is stored as
+            ## `datetime64[ns]` (you know, pandas) and arguments are
+            ## passed as `date`. In this case, extracting dt.date
+            ## recovers a `date` to make the comparison valid.
             df = df[df["date"].dt.date >= min_date]
     if max_date:
         try:
